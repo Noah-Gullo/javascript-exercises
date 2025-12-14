b716fae0846a4bfca8f5dddb874c812a63df845f
Noah Gullo <noahgulloingram@gmail.com>
2025-12-14 10:08:24 -0800

    Reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = [];
+    for(const char of string){
+        reversed.unshift(char);
+    }
+    return reversed.join("");
 };
 
 // Do not edit below this line
