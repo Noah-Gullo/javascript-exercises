ec0a6bb98c3c9a4854614f15cf1b3427e9486beb
Noah Gullo <noahgulloingram@gmail.com>
2025-12-14 10:01:23 -0800

    Repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let word = string;
+    if(num < 0){
+        return "ERROR";    
+    }else if(num === 0){
+        return "";
+    }
 
+    for(let i = 1; i < num; i++){
+        string += word;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
