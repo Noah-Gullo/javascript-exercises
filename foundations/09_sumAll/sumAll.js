96e2a341f469d6cbc4a18fcb9ec6538884d0bfd5
Noah Gullo <noahgulloingram@gmail.com>
2025-12-14 10:40:24 -0800

    Sum all

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a % 10 != 0 || b % 10 != 0){
+        return "ERROR";
+    }
 
+    let small = a > b ? b : a;
+    let big = a > b? a : b;
+    let length = big - small;
+    let sum = 0
+
+    for(let i = small; i < big; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
