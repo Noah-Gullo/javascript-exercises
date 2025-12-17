556c67d258de94bec9579a2b5b49b0a4e3b6bfa3
Noah Gullo <noahgulloingram@gmail.com>
2025-12-17 10:15:17 -0800

    find the oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = {};
 
+    for(let i = 0; i < people.length; i++){
+        if(people[i].yearOfDeath == undefined){
+            people[i].yearOfDeath = 2025;
+        }
+        
+        if(people[i].yearOfDeath - people[i].yearOfBirth > oldestAge){
+            oldestAge = people[i].yearOfDeath - people[i].yearOfBirth
+            oldestPerson = people[i]
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
