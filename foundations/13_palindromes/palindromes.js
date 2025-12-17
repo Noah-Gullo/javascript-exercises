410556d8897b2b155137cfc893f01c699dc975cb
Noah Gullo <noahgulloingram@gmail.com>
2025-12-17 09:49:34 -0800

    palikndrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const valid = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    const convert = string.toLowerCase().split("").filter((char) => valid.includes(char)).join("");
+
+    const reversed = convert.split("").reverse().join("");
+
+    return reversed === convert;
 };
 
 // Do not edit below this line
