b49d7390432e31d90c8a543adbd96049807b0ba4
Noah Gullo <noahgulloingram@gmail.com>
2025-12-17 10:07:16 -0800

    getTheTitles

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(typeof num !== "number"){
+        parseInt(num);
+    }
 
+    if (num < 0){
+        return "OOPS";
+    }else if(num === 0 || num === "0"){
+        return 0;
+    }
+
+
+    let previous = 1;
+    let beforePrevious = 0;
+
+    for(let i = 2; i <= num; i++){
+        let curr = previous + beforePrevious;
+        beforePrevious = previous;
+        previous = curr;
+    }
+
+    return previous;
 };
 
 // Do not edit below this line
